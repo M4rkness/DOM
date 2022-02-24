df5f24235b21f2d0f9ddd97e613e7c433b571aef
M4rkness <bism4rk1337@gmail.com>
2022-02-24 23:05:35 +0600

    DOM practic

--- js/script.js
+++ js/script.js
@@ -1,19 +1,51 @@
 "use strict";
 
-const box = document.getElementById("box");
-console.log(box);
+const box = document.getElementById("box"),
+        btns = document.getElementsByTagName("button"),
+        circles = document.getElementsByClassName("circle"),
+        heart_block = document.querySelector(".heart_block"),
+        hearts = heart_block.querySelectorAll(".heart"),
+        oneHeart = heart_block.querySelector(".heart");
 
-const btns = document.getElementsByTagName("button");
-console.log(btns[0]);
 
-const circles = document.getElementsByClassName("circle");
-console.log(circles);
+// box.style.backgroundColor = "blue";
+// box.style.width = "500px";
+box.style.cssText = "background-color: blue; width: 500px";
 
-const hearts = document.querySelectorAll(".heart");
+
+btns[1].style.borderRadius = "100%";
+circles[0].style.backgroundColor = "red";
+
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = "blue"
+// };
 
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = "blue"
 });
 
-const oneHeart = document.querySelector(".heart");
-console.log(oneHeart);
+const div = document.createElement("div");
+// const text = document.createTextNode("Тут был я");
+
+div.classList.add("black");
+div.style.cssText = "background-color: black; width: 200px; height: 100px; color: white;";
+
+heart_block.append(div);
+// heart_block.appendChild(div); // todo устаревший 
+
+// heart_block.prepend(div);
+// hearts[0].before(div);
+// hearts[0].after(div);
+// heart_block.insertBefore(div, hearts[2]); // todo устаревший
+
+// circles[0].remove();
+// heart_block.removeChild(hearts[1]); // todo устаревший
+
+hearts[0].replaceWith(circles[0]);
+// heart_block.replaceChild(circles[0], hearts[2]); // todo устаревший
+
+div.innerHTML = "<h1>Hello World!</h1>";
+
+// div.textContent = "Hello world!";
+
+div.insertAdjacentHTML("afterbegin", "<h2>Hello</h2>");
